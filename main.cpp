c74e94c9af628b3ef86e176eb5f5f3b79c80fcfc
jose <rodolfojose@live.com>
2018-08-10 20:11:34 -0600

    imprime mi automata con sus estados y simbolos pero sin las transiciones y comentado el main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main(){
     Automata * automa = new Automata(); //instancia de la clase de automata
-    cout<<"DFA"<<endl;
     int cantidadEstados; //cantidad de estados
     int cantidadSimbolos; //cantidad de simbolos
     vector<string> simbolosA; // arreglo de vectores de los simbolos del alfabeto
@@ -19,30 +18,33 @@ int main(){
         simbolosA.push_back(s);
     }
     automa->simbolosPossibles(cantidadSimbolos,simbolosA); //llamo la funcion para agregar mi arreglo al arregle de simbolos del autoamta
+
     string **table; // instancia de tabla para el print del automata
-    table = new string*[cantidadEstados]; //instancia de un apuntador de la tabla a los estados
-    for(int a = 0; a<cantidadEstados; a++){ // for para agregar al otro apuntador de la tabla los simbolos
-        table[a] = new string[cantidadSimbolos];
+
+    table = new string*[cantidadEstados+1]; //instancia de un apuntador de la tabla a los estados
+
+    for(int a = 0; a<cantidadEstados+1; a++){ // for para agregar al otro apuntador de la tabla los simbolos
+        table[a] = new string[cantidadSimbolos+1];
     }
-    table[0][0] = "-";
+    table[0][0] = "*";
 
     Estado *EstadoNuevo;//instancia para ingresar un estado nuevo al automata
 
-    for(int a = 0; a<cantidadEstados; a++){ //for para poder colocar el estado a la tabla y ingresando el numero del estado asi como ingresandolo a vector de los estados del automata
+    for(int a = 1; a<cantidadEstados+1; a++){ //for para poder colocar el estado a la tabla y ingresando el numero del estado asi como ingresandolo a vector de los estados del automata
         table[a][0] = to_string(a-1);
         EstadoNuevo = new Estado(to_string(a-1));
         automa->estadosA.push_back(EstadoNuevo); 
     }
 
-    for(int b = 0; b<cantidadSimbolos; b++){ //for para colocar los simbolos a los estados
+    for(int b = 1; b<cantidadSimbolos+1; b++){ //for para colocar los simbolos a los estados
         table[0][b] = simbolosA[b-1];
     }
     for(int a = 1; a<=cantidadEstados; a++){ //los for son para colocar el los valore default de cada estado con el simbolo
         for(int b = 1; b<=cantidadSimbolos; b++){
-            table[a][b] = "-";
+            table[a][b] = "*";
         }
     }
-        
+    
     for(int a = 0; a<cantidadEstados+1; a++){
         for(int b = 0; b<cantidadSimbolos+1; b++){
             if(b!=cantidadSimbolos){
@@ -52,7 +54,8 @@ int main(){
             }
                         
         }
-    }    
+    }          
+
 
 
 
