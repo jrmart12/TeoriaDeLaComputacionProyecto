e1f337c95757571927500ecad6432bc453e5fe8e
jose <rodolfojose@live.com>
2018-08-28 23:27:09 -0600

    ingresar el nfa solo falta juntar los estados a los que iria

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main(){
+    cout<< "1. DFA\n";
+    cout<< "2. NFA\n";
+    cout<< "3. NFAe\n";
+    int eleccion;
+    cin >> eleccion;
+    switch (eleccion)
+    {
+case 1: //DFA
+{
     Automata * automa = new Automata(); //instancia de la clase de automata
     int cantidadEstados; //cantidad de estados
     int cantidadSimbolos; //cantidad de simbolos
@@ -12,7 +21,7 @@ int main(){
     cin>>cantidadEstados;
     cout<<"cantidad de simbolos: ";
     cin>>cantidadSimbolos;
-    cout<<"simbolos: ";
+    cout<<"simbolos: \n";
     for(int x=0; x<cantidadSimbolos;x++){ //for de 0 a la cantidad de simbolos para meterlo al arreglo de simbolos
         cin>>s;
         simbolosA.push_back(s);
@@ -107,6 +116,101 @@ int main(){
     cout<<"Ingrese nombre del archivo: ";
     cin>>filename;
     leer->leerCadenaDeArchivo(automa,filename);
+    break;
+}
+    case 2:
+    {
+    Automata * automa = new Automata(); //instancia de la clase de automata
+    int cantidadEstados; //cantidad de estados
+    int cantidadSimbolos; //cantidad de simbolos
+    vector<string> simbolosA; // arreglo de vectores de los simbolos del alfabeto
+    string s; 
+    cout<<"cantidad de estados: ";
+    cin>>cantidadEstados;
+    cout<<"cantidad de simbolos: ";
+    cin>>cantidadSimbolos;
+    cout<<"simbolos: \n";
+    for(int x=0; x<cantidadSimbolos;x++){ //for de 0 a la cantidad de simbolos para meterlo al arreglo de simbolos
+        cin>>s;
+        simbolosA.push_back(s);
+    }
+    automa->simbolosPossibles(cantidadSimbolos,simbolosA); //llamo la funcion para agregar mi arreglo al arregle de simbolos del autoamta
 
+    string **table; // instancia de tabla para el print del automata
+
+    table = new string*[cantidadEstados+1]; //instancia de un apuntador de la tabla a los estados
+
+    for(int a = 0; a<cantidadEstados+1; a++){ // for para agregar al otro apuntador de la tabla los simbolos
+        table[a] = new string[cantidadSimbolos+1];
+    }
+    table[0][0] = "*";
+
+    Estado *EstadoNuevo;//instancia para ingresar un estado nuevo al automata
+
+    for(int a = 1; a<cantidadEstados+1; a++){ //for para poder colocar el estado a la tabla y ingresando el numero del estado asi como ingresandolo a vector de los estados del automata
+        table[a][0] = to_string(a-1);
+        EstadoNuevo = new Estado(to_string(a-1));
+        automa->estadosA.push_back(EstadoNuevo); 
+    }
+
+    for(int b = 1; b<cantidadSimbolos+1; b++){ //for para colocar los simbolos a los estados
+        table[0][b] = simbolosA[b-1];
+    }
+    for(int a = 1; a<=cantidadEstados; a++){ //los for son para colocar el los valore default de cada estado con el simbolo
+        for(int b = 1; b<=cantidadSimbolos; b++){
+            table[a][b] = "*";
+        }
+    }
+    
+    for(int a = 0; a<cantidadEstados+1; a++){ //for para la impresion de la tabla del automata
+        for(int b = 0; b<cantidadSimbolos+1; b++){
+            if(b!=cantidadSimbolos){
+                cout<<table[a][b]<<' ';
+            } else{
+                cout<<table[a][b]<<endl;
+            }
+                        
+        }
+    }  
+
+    int estadoInicial;
+    int cantidadEstadoFinal;  
+    vector<Estado *> estadoFinal;
+
+    //Estado Inicial
+    cout<<"Estado Inicial:";
+    cin>>estadoInicial; //agarrar el estado inicial del usuario
+    Estado * inicial = automa->getEstado(estadoInicial); //agarro el estado inicial y se lo agrego a una variable
+    automa->setEstadoInicial(inicial); //seteo el estado del usuario final al automata
+
+    //Estado Final
+    cout<<"Cantidad de Estado Finales: ";
+    cin>>cantidadEstadoFinal; //agarrar la cantidad estado final del usuario
+    cout<<"Estado Finales: ";
+    int temporal;
+    for(int cont=0;cont<cantidadEstadoFinal;cont++){ //for para agregar los estados finales al vector finales
+        cin>>temporal;
+        Estado * finales= automa->getEstado(temporal);
+        estadoFinal.push_back(finales);
+    }
+    automa->setEstadosFinal(estadoFinal);        
+                
+    vector<Arista *> crearEstados;
+    int cantTransiciones;
+    vector<string> cualesIria;
+    int cantEstadosJuntados;    
+    for(int x=0;x<cantidadEstados;a++){
+        
+    }
+    break;
+    }
+
+    case 3:
+    {
+
+    break;
+    }
+
+}    
 
 }
